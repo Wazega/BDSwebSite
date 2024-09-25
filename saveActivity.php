7e0003ced78dc0969aa1d323d17984ed7470e91d
Gabin <gabin.dubois@etu.unistra.fr>
2024-09-25 16:29:47 +0200

    modif de acitivies.php on y croit

--- saveActivity.php
+++ saveActivity.php
@@ -4,6 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo "<pre>";
+print_r($_POST);
+print_r($_FILES);
+echo "</pre>";
+exit;
+
+
 // Vérification des données POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
